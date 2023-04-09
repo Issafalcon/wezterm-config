ff79116387c2e453d861eeace9c7d9b3c435c49e
issafalcon <atfiggins@gmail.com>
2023-04-09 19:02:37 +0100

    feat(launchmenu): Adds powershell admin and vs tools to launch menu

--- menus/launchmenu.lua
+++ menus/launchmenu.lua
@@ -1,7 +1,34 @@
-local launchmenu = {}
+local wezterm = require("wezterm")
+local launch = {}
 
-function launchmenu.apply_to_config(config)
-    config.launch_menu = { }
+function launch.apply_to_config(config)
+	local launch_menu = {}
+	if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+		table.insert(launch_menu, {
+			label = "PowerShell Core - Admin",
+			args = {
+				"pwsh",
+				"-Command",
+				"& {Start-Process pwsh -Verb RunAs}",
+			},
+		})
+
+		-- Find installed visual studio version(s) and add their compilation
+		-- environment command prompts to the menu
+		for _, vsvers in ipairs(wezterm.glob("Microsoft Visual Studio/20*", "C:/Program Files (x86)")) do
+			local year = vsvers:gsub("Microsoft Visual Studio/", "")
+			table.insert(launch_menu, {
+				label = "x64 Native Tools VS " .. year,
+				args = {
+					"cmd.exe",
+					"/k",
+					"C:/Program Files (x86)/" .. vsvers .. "/BuildTools/VC/Auxiliary/Build/vcvars64.bat",
+				},
+			})
+		end
+	end
+
+	config.launch_menu = launch_menu
 end
 
-return launchmenu
+return launch
