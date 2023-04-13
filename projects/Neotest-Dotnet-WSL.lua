fff3d83bc3f6a4401ebd79578b5915c6eb5355ea
issafalcon <atfiggins@gmail.com>
2023-04-13 21:12:22 +0100

    feat(project): Neotest dotnet project added

---
+++ projects/Neotest-Dotnet-WSL.lua
@@ -0,0 +1,31 @@
+local M = {}
+
+function M.startup(wezterm, workspace_name)
+	local mux = wezterm.mux
+	local project_dir = wezterm.home_dir .. "/repos"
+
+	local neotest_dotnet_tab, neotest_dotnet_pane, proj_window = mux.spawn_window({
+		workspace = workspace_name,
+		cwd = project_dir,
+		args = {
+			"wsl",
+		},
+	})
+
+	neotest_dotnet_tab:set_title("neotest-dotnet")
+	neotest_dotnet_pane:send_text("tmux_start_neotest_dotnet.sh\r")
+
+	local learning_dotnet_tab, learning_dotnet_pane, proj_window = mux.spawn_window({
+		workspace = workspace_name,
+		cwd = project_dir,
+		args = {
+			"wsl",
+		},
+	})
+
+	learning_dotnet_tab:set_title("learning-dotnet")
+	learning_dotnet_pane:send_text("tmux_start_learning_dotnet.sh\r")
+
+	mux.set_active_workspace(workspace_name)
+end
+return M
