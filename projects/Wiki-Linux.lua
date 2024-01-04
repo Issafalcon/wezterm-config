3ae22e1545f06054eef0c2f6c393bbf274eb051a
Adam Figgins <atfiggins@gmail.com>
2024-01-04 19:37:50 +0000

    Projects - Updating wiki-linux project startup
    - Adds cloning of obsidian repo if it doesn't exist

--- projects/Wiki-Linux.lua
+++ projects/Wiki-Linux.lua
@@ -4,14 +4,31 @@ function M.startup(wezterm, workspace_name)
   local mux = wezterm.mux
   local project_dir = wezterm.home_dir .. "/repos"
 
-  -- Wiki repo tab (Neovim)
-  local first_tab, first_pane, proj_window = mux.spawn_window({
-    workspace = workspace_name,
-    cwd = project_dir,
+  -- Check if ~/repos/wiki-md exists, and if not, clone it
+  local wiki_md_dir = project_dir .. "/wiki-md"
+
+  local wiki_md_exists = wezterm.run_child_process({
+    "test",
+    "-d",
+    wiki_md_dir,
   })
 
-  first_tab:set_title("Wiki")
-  first_pane:send_text("cd wiki\r")
+  if wiki_md_exists ~= 0 then
+    wezterm.run_child_process({
+      "git",
+      "clone",
+      "https://github.com/Issafalcon/wiki-md.git",
+      wiki_md_dir,
+    })
+  end
+
+  -- Obsidian Wiki repo tab (Neovim)
+  local first_tab, first_pane, proj_window = mux.spawn_window({
+    workspace = workspace_name,
+    cwd = wiki_md_dir,
+  })
+
+  first_tab:set_title("Obsidian")
   first_pane:send_text("nvim\r")
 
   -- Terminal tab
@@ -21,6 +38,15 @@ function M.startup(wezterm, workspace_name)
 
   second_tab:set_title("Terminals")
   second_pane:send_text("cd wiki\r")
+
+  -- Wiki repo tab (Neovim)
+  local third_tab, third_pane, _ = proj_window.spawn_window({
+    workspace = workspace_name,
+    cwd = project_dir .. "/wiki",
+  })
+
+  third_tab:set_title("Wiki")
+  third_pane:send_text("nvim\r")
 
   mux.set_active_workspace(workspace_name)
 end
