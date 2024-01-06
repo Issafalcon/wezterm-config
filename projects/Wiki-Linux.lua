fd64a17080604259e0f6ce8227f1f2c51522ce94
Adam Figgins <adam@pop-os.localdomain>
2024-01-06 18:09:32 +0000

    Projects - Fixing Wiki-Linux script

--- projects/Wiki-Linux.lua
+++ projects/Wiki-Linux.lua
@@ -29,24 +29,29 @@ function M.startup(wezterm, workspace_name)
   })
 
   first_tab:set_title("Obsidian")
-  first_pane:send_text("nvim\r")
-
-  -- Terminal tab
-  local second_tab, second_pane, _ = proj_window:spawn_tab({
-    cwd = project_dir,
-  })
-
-  second_tab:set_title("Terminals")
-  second_pane:send_text("cd wiki\r")
 
   -- Wiki repo tab (Neovim)
-  local third_tab, third_pane, _ = proj_window:spawn_tab({
+  local second_tab, second_pane, _ = proj_window:spawn_tab({
     workspace = workspace_name,
     cwd = project_dir .. "/wiki",
   })
 
-  third_tab:set_title("Wiki")
-  third_pane:send_text("nvim\r")
+  second_tab:set_title("Wiki")
+  second_pane:send_text("nvim\r")
+
+  -- Terminal tab
+  local third_tab, third_pane, _ = proj_window:spawn_tab({
+    cwd = project_dir,
+  })
+
+  third_tab:set_title("Terminals")
+
+  local lower_split = third_pane:split({
+    direction = "Bottom",
+    size = 0.05,
+  })
+
+  lower_split:send_text("obsidian\r")
 
   mux.set_active_workspace(workspace_name)
 end
