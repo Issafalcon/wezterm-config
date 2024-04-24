df7d97d0e1d67eac8eef272cf7d83b7084f9aeee
Adam Figgins <adam.figgins@wedo-digital.co.uk>
2024-04-24 14:46:18 +0100

    feat(project): Adds scrimmage project

---
+++ projects/Scrimmage-WSL.lua
@@ -0,0 +1,19 @@
+local M = {}
+
+function M.startup(wezterm, workspace_name)
+  local mux = wezterm.mux
+  local project_dir = wezterm.home_dir .. "/repos"
+
+  local wsl_tab, wsl_pane, proj_window = mux.spawn_window({
+    workspace = workspace_name,
+    cwd = project_dir,
+    args = { "wsl" },
+  })
+
+  wsl_tab:set_title("Scrimmage")
+  wsl_pane:send_text("tmux_start_scrimmage.sh\r")
+
+  mux.set_active_workspace(workspace_name)
+end
+
+return M
