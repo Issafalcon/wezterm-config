53bac46631f0821e1705f75970038c4e22d3c87c
Adam Figgins <adam.figgins@wedo-digital.co.uk>
2024-06-24 08:23:58 +0100

    feat(projects): Wsl config project with test

--- projects/ConfigFiles-WSL.lua
+++ projects/ConfigFiles-WSL.lua
@@ -2,18 +2,39 @@ local M = {}
 
 function M.startup(wezterm, workspace_name)
   local mux = wezterm.mux
-  local project_dir = wezterm.home_dir .. "/repos"
 
-  local wsl_tab, wsl_pane, proj_window = mux.spawn_window({
+  local first_tab, first_pane, proj_window = mux.spawn_window({
     workspace = workspace_name,
-    cwd = project_dir,
     args = { "wsl" },
   })
 
-  wsl_tab:set_title("WSL - Config")
-  wsl_pane:send_text("tmux_start_dotfiles.sh\r")
+  first_tab:set_title("dotfiles")
+  first_pane:send_text('cd "~/dotfiles"\r')
+
+  local second_tab, second_pane, _ = proj_window:spawn_tab({
+    args = { "wsl" },
+  })
+
+  second_tab:set_title("Nvim Config")
+  second_pane:send_text('cd "$DOTFILES/nvim/.config/nvim"\r')
+
+  local third_tab, third_pane, _ = proj_window:spawn_tab({
+    args = { "wsl" },
+  })
+
+  third_tab:set_title("WezTerm Config")
+  third_pane:send_text('cd "$DOTFILES/wezterm/.config/wezterm"\r')
 
   mux.set_active_workspace(workspace_name)
+
+  local forth_tab, forth_pane, _ = proj_window:spawn_tab({
+    args = { "wsl" },
+  })
+
+  forth_tab:set_title("Test")
+  forth_pane:send_text('if [ ! -d "${PROJECTS}"/learning-dotnet ]; then\r')
+  forth_pane:send_text('git clone https://github.com/Issafalcon/learning-dotnet.git "${PROJECTS}"/learning-dotnet\r')
+  forth_pane:send_text("fi\r")
 end
 
 return M
--- ui/init.lua
+++ ui/init.lua
@@ -4,12 +4,12 @@ local windows = require("ui.windows")
 local ui = {}
 
 function ui.apply_to_config(config)
-	config.use_fancy_tab_bar = false
-	config.tab_max_width = 24
+  config.use_fancy_tab_bar = false
+  config.tab_max_width = 24
 
-	colors.apply_to_config(config)
-	fonts.apply_to_config(config)
-	windows.apply_to_config(config)
+  colors.apply_to_config(config)
+  fonts.apply_to_config(config)
+  windows.apply_to_config(config)
 end
 
 return ui
