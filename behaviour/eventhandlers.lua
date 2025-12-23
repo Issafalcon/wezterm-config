c0eb4f963d20ff354717d7beef48111f556bcf9e
ukafig <adam_figgins@waters.com>
2025-12-23 16:19:35 +0000

    UI - Updating tabline ui

--- behaviour/eventhandlers.lua
+++ behaviour/eventhandlers.lua
@@ -8,122 +8,114 @@ local config = {}
 
 ---@diagnostic disable-next-line: unused-local
 local function create_tab_title(tab, tabs, panes, config, hover, max_width)
-	local user_title = tab.tab_title
+  local user_title = tab.tab_title
 
-	if user_title and #user_title > 0 then
-		return tab.tab_index + 1 .. ":" .. user_title
-	end
+  if user_title and #user_title > 0 then return tab.tab_index + 1 .. ":" .. user_title end
 
-	local title = wezterm.truncate_right(path_utils.basename(tab.active_pane.foreground_process_name), max_width)
-	if title == "" then
-		local dir = string.gsub(tab.active_pane.title, "(.*[: ])(.*)]", "%2")
-		dir = path_utils.convert_useful_path(dir)
-		title = wezterm.truncate_right(dir, max_width)
-	end
+  local title = wezterm.truncate_right(path_utils.basename(tab.active_pane.foreground_process_name), max_width)
+  if title == "" then
+    local dir = string.gsub(tab.active_pane.title, "(.*[: ])(.*)]", "%2")
+    dir = path_utils.convert_useful_path(dir)
+    title = wezterm.truncate_right(dir, max_width)
+  end
 
-	local copy_mode, n = string.gsub(tab.active_pane.title, "(.+) mode: .*", "%1", 1)
-	if copy_mode == nil or n == 0 then
-		copy_mode = ""
-	else
-		copy_mode = copy_mode .. ": "
-	end
-	return copy_mode .. tab.tab_index + 1 .. ":" .. title
+  local copy_mode, n = string.gsub(tab.active_pane.title, "(.+) mode: .*", "%1", 1)
+  if copy_mode == nil or n == 0 then
+    copy_mode = ""
+  else
+    copy_mode = copy_mode .. ": "
+  end
+  return copy_mode .. tab.tab_index + 1 .. ":" .. title
 end
 
 ---@diagnostic disable-next-line: unused-local
 local function update_ssh_status(window, pane)
-	local text = pane:get_domain_name()
-	if text == "local" then
-		text = ""
-	end
-	return {
-		{ Attribute = { Italic = true } },
-		{ Text = text .. " " },
-	}
+  local text = pane:get_domain_name()
+  if text == "local" then text = "" end
+  return {
+    { Attribute = { Italic = true } },
+    { Text = text .. " " },
+  }
 end
 
 ---@diagnostic disable-next-line: unused-local
 local function display_copy_mode(window, pane)
-	local name = window:active_key_table()
-	if name then
-		name = "Mode: " .. name
-	end
-	return { { Attribute = { Italic = false } }, { Text = name or "" } }
+  local name = window:active_key_table()
+  if name then name = "Mode: " .. name end
+  return { { Attribute = { Italic = false } }, { Text = name or "" } }
 end
 
 wezterm.on("gui-startup", function(cmd)
-	local tab, pane, window = mux.spawn_window(cmd or {})
-	window:gui_window():maximize()
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
 end)
 
-wezterm.on("update-right-status", function(window, pane)
-	-- local tmux = update_tmux_style_tab(window, pane)
-	local ssh = update_ssh_status(window, pane)
-	local copy_mode = display_copy_mode(window, pane)
-	local status = tables_utils.merge_lists(ssh, copy_mode)
-	window:set_right_status(wezterm.format(status))
-end)
+-- wezterm.on("update-right-status", function(window, pane)
+-- 	-- local tmux = update_tmux_style_tab(window, pane)
+-- 	local ssh = update_ssh_status(window, pane)
+-- 	local copy_mode = display_copy_mode(window, pane)
+-- 	local status = tables_utils.merge_lists(ssh, copy_mode)
+-- 	window:set_right_status(wezterm.format(status))
+-- end)
 
 wezterm.on("trigger-nvim-with-scrollback", function(window, pane)
-	local scrollback = pane:get_lines_as_text()
-	local name = os.tmpname()
-	local f = io.open(name, "w+")
-	if f == nil then
-		return
-	end
+  local scrollback = pane:get_lines_as_text()
+  local name = os.tmpname()
+  local f = io.open(name, "w+")
+  if f == nil then return end
 
-	f:write(scrollback)
-	f:flush()
-	f:close()
-	window:perform_action(
-		act({
-			SpawnCommandInNewTab = {
-				args = { "nvim", name },
-			},
-		}),
-		pane
-	)
-	wezterm.sleep_ms(3000)
-	os.remove(name)
+  f:write(scrollback)
+  f:flush()
+  f:close()
+  window:perform_action(
+    act({
+      SpawnCommandInNewTab = {
+        args = { "nvim", name },
+      },
+    }),
+    pane
+  )
+  wezterm.sleep_ms(3000)
+  os.remove(name)
 end)
 
 ---@diagnostic disable-next-line: unused-local
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-	local title = create_tab_title(tab, tabs, panes, config, hover, max_width)
+  local title = create_tab_title(tab, tabs, panes, config, hover, max_width)
 
-	-- selene: allow(undefined_variable)
-	local solid_left_arrow = utf8.char(0x2590)
-	-- selene: allow(undefined_variable)
-	local solid_right_arrow = utf8.char(0x258c)
-	-- https://github.com/wez/wezterm/issues/807
-	-- local edge_background = scheme.background
-	-- https://github.com/wez/wezterm/blob/61f01f6ed75a04d40af9ea49aa0afe91f08cb6bd/config/src/color.rs#L245
-	local edge_background = "#2e3440"
-	local background = scheme.ansi[1]
-	local foreground = scheme.ansi[5]
+  -- selene: allow(undefined_variable)
+  local solid_left_arrow = utf8.char(0x2590)
+  -- selene: allow(undefined_variable)
+  local solid_right_arrow = utf8.char(0x258c)
+  -- https://github.com/wez/wezterm/issues/807
+  -- local edge_background = scheme.background
+  -- https://github.com/wez/wezterm/blob/61f01f6ed75a04d40af9ea49aa0afe91f08cb6bd/config/src/color.rs#L245
+  local edge_background = "#2e3440"
+  local background = scheme.ansi[1]
+  local foreground = scheme.ansi[5]
 
-	if tab.is_active then
-		background = scheme.brights[1]
-		foreground = scheme.brights[8]
-	elseif hover then
-		background = scheme.cursor_bg
-		foreground = scheme.cursor_fg
-	end
-	local edge_foreground = background
+  if tab.is_active then
+    background = scheme.brights[1]
+    foreground = scheme.brights[8]
+  elseif hover then
+    background = scheme.cursor_bg
+    foreground = scheme.cursor_fg
+  end
+  local edge_foreground = background
 
-	return {
-		{ Attribute = { Intensity = "Bold" } },
-		{ Background = { Color = edge_background } },
-		{ Foreground = { Color = edge_foreground } },
-		{ Text = solid_left_arrow },
-		{ Background = { Color = background } },
-		{ Foreground = { Color = foreground } },
-		{ Text = title },
-		{ Background = { Color = edge_background } },
-		{ Foreground = { Color = edge_foreground } },
-		{ Text = solid_right_arrow },
-		{ Attribute = { Intensity = "Normal" } },
-	}
+  return {
+    { Attribute = { Intensity = "Bold" } },
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+    { Text = solid_left_arrow },
+    { Background = { Color = background } },
+    { Foreground = { Color = foreground } },
+    { Text = title },
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+    { Text = solid_right_arrow },
+    { Attribute = { Intensity = "Normal" } },
+  }
 end)
 
 return config
